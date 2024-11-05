458b78e8d384c70cddee87b5f4b56511233e6f49
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-11-05 07:34:48 -0800

    Added About Us and Our Mission sections to landing page

--- src/pages/LandingPage/sections/OurMission.jsx
+++ src/pages/LandingPage/sections/OurMission.jsx
@@ -1,14 +1,22 @@
-// AboutUs.jsx
-import { aboutImage } from "../../../assets";
+import React from "react";
+import { missionImage } from "../../../assets";
 
-const AboutUs = () => {
+const OurMission = () => {
   return (
-    <section id="about" className="py-10">
+    <section id="mission" className="py-10">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col lg:flex-row items-center">
-          {/* About Us Text */}
+        <div className="flex flex-col lg:flex-row gap-10 items-center">
+          <div className="lg:w-1/2">
+            <div className="shadow-lg rounded-lg overflow-hidden">
+              <img
+                src={missionImage}
+                className="w-full h-auto"
+                alt="About Us Image"
+              />
+            </div>
+          </div>
           <div className="lg:w-1/2 mb-8 lg:mb-0">
-            <h2 className="text-2xl font-bold">About Us</h2>
+            <h2 className="text-2xl font-bold">Our Mission</h2>
             <p className="text-gray-600 mt-4">
               We are Innovate, a forward-thinking organization dedicated to
               transforming STEM education for girls. Our mission is to make STEM
@@ -21,16 +29,10 @@ const AboutUs = () => {
               and pace.
             </p>
           </div>
-          {/* About Us Image */}
-          <div className="lg:w-1/2">
-            <div className="shadow-lg rounded-lg overflow-hidden">
-              <img src={aboutImage} className="w-full h-auto" alt="About Us" />
-            </div>
-          </div>
         </div>
       </div>
     </section>
   );
 };
 
-export default AboutUs;
+export default OurMission;
