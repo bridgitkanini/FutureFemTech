7f53827f0c65ce84abb04700fd54d40dce14095a
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-11-06 18:53:00 +0300

    Added map to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,40 @@ import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className=" bg-yellow-500 bg-opacity-70 backdrop-blur-lg bottom-0 left-0 right-0 py-3 px-3 md:px-10">
-      <div className="flex flex-row justify-between px-16">
-        <h2 className="text-lg font-bold text-black">FutureFemTeck</h2>
+      <div className="flex flex-col gap-5 md:flex-row md:justify-between px-8 md:px-16 items-start md:items-center">
         <div>
+          <h2 className="text-2xl font-bold text-black my-4">FutureFemTeck</h2>
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8594529691713!2d36.79364937496568!3d-1.2561717987318686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f176f89893739%3A0x6677026bfae79ba7!2sThe%20Piano!5e0!3m2!1sen!2ske!4v1730904908935!5m2!1sen!2ske"
+              width="300"
+              height="250"
+              style={{ border: 0 }}
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 md:gap-4 text-xl font-semibold">
           <p>Careers</p>
           <p>Mentorship</p>
           <p>Scholarships</p>
         </div>
-        <div>
+        <div className="flex flex-col gap-2 md:gap-4 text-xl font-semibold">
           <p>Contacts:</p>
           <p>info@futurefemtech.com</p>
           <p>+254 712 345678</p>
         </div>
       </div>
       <hr className="my-3" />
-      <div className="flex flex-row justify-between px-16">
+      <div className="flex flex-row justify-between px-4 md:px-16 md:mx-10">
         <p>&copy; 2024 FutureFemTech, Inc. All Rights Reserved.</p>
         <div className="flex flex-row gap-9">
-          <FaFacebook />
-          <FaInstagram />
-          <FaXTwitter />
-          <FaYoutube />
+          <FaFacebook className="text-3xl" />
+          <FaInstagram className="text-3xl" />
+          <FaXTwitter className="text-3xl" />
+          <FaYoutube className="text-3xl" />
         </div>
       </div>
     </div>
